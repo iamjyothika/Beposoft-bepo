a216a082d01c0b6f51bff7f39aa036d500c28bf0
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-01-12 10:35:15 +0530

    dummy

--- beposoft/settings.py
+++ beposoft/settings.py
@@ -156,7 +156,7 @@ USE_I18N = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/hbrhbrjhbrt
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'  
 MEDIA_URL = '/media/'    
