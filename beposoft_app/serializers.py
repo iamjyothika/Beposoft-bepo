e1eea7fe7493e236d0121a7969deb46734dda3c4
Muhammad Fuhad MK <muhammad.fuhad.dev@gmail.com>
2025-01-15 15:59:19 +0530

    updation in expense getting

--- beposoft_app/serializers.py
+++ beposoft_app/serializers.py
@@ -362,6 +362,11 @@ class BankSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bank
         fields = "__all__"
+
+class Bankserializers(serializers.ModelSerializer):
+    class Meta:
+        model = Bank
+        fields = ['id', 'name'] 
         
 
 class PaymentRecieptsViewSerializers(serializers.ModelSerializer):
@@ -726,10 +731,10 @@ class ExpenseSerializer(serializers.ModelSerializer):
 class ExpenseModelsSerializers(serializers.ModelSerializer):
     company = CompanyDetailsSerializer(read_only=True)
     payed_by = UserUpdateSerilizers(read_only=True)
-    banks = BankSerializer(read_only=True)
+    bank = Bankserializers(read_only=True)
     class Meta :
         model = ExpenseModel
-        fields = ['id','company','payed_by','banks','purpose_of_payment','amount','expense_date','transaction_id','description','added_by']
+        fields = ['id','company','payed_by','bank','purpose_of_payment','amount','expense_date','transaction_id','description','added_by']
         
         
         
