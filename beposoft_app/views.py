c19d32acc65c83000f39e1a7f9385a3d89376acd
iamjyothika <jyothikajothu13@gmail.com>
2025-02-08 10:02:11 +0530

    customer upload..

--- beposoft_app/views.py
+++ beposoft_app/views.py
@@ -384,7 +384,7 @@ class UserCustomerAddingView(BaseTokenView):
 
 
 class CustomerPagination(PageNumberPagination):
-    page_size = 1000  # ✅ Default customers per page
+    page_size = 500  # ✅ Default customers per page
     page_size_query_param = 'page_size'  # ✅ Allows dynamic page size
     max_page_size = 5000 # ✅ Limits large queries
 
